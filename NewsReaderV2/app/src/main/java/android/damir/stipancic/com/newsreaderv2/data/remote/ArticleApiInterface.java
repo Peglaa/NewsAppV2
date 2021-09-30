42dfd7312f0cda10a1b0e6aae8ac8514c545ae68
Pegla <stipa.damir@hotmail.com>
2021-09-30 14:24:06 +0200

    Misc changes
    Misc changes

--- NewsReaderV2/app/src/main/java/android/damir/stipancic/com/newsreaderv2/data/remote/ArticleApiInterface.java
+++ NewsReaderV2/app/src/main/java/android/damir/stipancic/com/newsreaderv2/data/remote/ArticleApiInterface.java
@@ -1,7 +1,5 @@
 package android.damir.stipancic.com.newsreaderv2.data.remote;
 
-import android.damir.stipancic.com.newsreaderv2.data.remote.ArticleListDTO;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
